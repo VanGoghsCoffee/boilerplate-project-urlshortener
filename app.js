88c77b8ef52f5389b3365b20996c8e0f31ffec35
David Rochholz <mail@davidrochholz.de>
2022-01-03 22:40:17 +0100

    shortening urls

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const shortUrls = [];
+const successResObj = (originalUrl, shortUrl) => ({ original_url: originalUrl, short_url: shortUrl });
+const invalidResObj = { error: "invalid url" };
+
+function handleInvalidUrl(req, res, next) {
+    const url = req.body["url"];
+    req.isInvalid = false;
+
+    if (url === "")
+        req.isInvalid = true;
+    
+    try {
+        new URL(url);
+    } catch (TypeError) {
+        req.isInvalid = true;
+    }
+    next();
+}
+
+function handleShortUrl(req, res, next) {
+    const url = req.body["url"];
+
+    if (req.isInvalid) {
+        res.json(invalidResObj);
+    } else {
+        shortUrls.push(url);
+        res.json(successResObj(url, shortUrls.length));
+    }
+}
+
+function handleRedirect(req, res, next) {
+    const shortUrl = req.params.short_url;
+
+    if (shortUrl === undefined || shortUrl > shortUrls.length) {
+        res.json(invalidResObj)
+    } else {
+        res.redirect(shortUrls[shortUrl - 1]);
+    }
+}
+
+app.route("/api/shorturl")
+    .post([handleInvalidUrl, handleShortUrl]);
+
+app.route("/api/shorturl/:short_url")
+    .get(handleRedirect);
+
+module.exports = app;
